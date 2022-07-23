3a9385eab2078bd925e731fd5741957ff1f1976f
marekbobrowski <gm.bobrowski@gmail.com>
2022-07-23 20:36:57 +0200

    Fix the update of sound delay, arrange code into methods (mostly default Steinberg code)

--- src/processor.cpp
+++ src/processor.cpp
@@ -60,16 +60,75 @@ tresult PLUGIN_API Processor::setActive (TBool state)
 
 tresult PLUGIN_API Processor::process (Vst::ProcessData& data)
 {
+	if (data.numInputs == 0 || data.numOutputs == 0)
+	{
+		return kResultOk;
+	}
+
+	void** in = getChannelBuffersPointer(processSetup, *data.inputs);
+	void** out = getChannelBuffersPointer(processSetup, *data.outputs);
+
+	if (data.inputs[0].silenceFlags != 0)
+	{
+		handleSilenceFlags(in, out, data);
+		return kResultOk;
+	}
+
+	handleParameterChange(data);
+
+	if (updateTempo(data))
+	{
+		calcSetDelay();
+	}
+
+	if (data.numSamples > 0)
+	{
+		processBuffer(in, out, data.numSamples);
+	}
+	return kResultOk;
+}
+
+
+tresult PLUGIN_API Processor::canProcessSampleSize (int32 symbolicSampleSize)
+{
+	if (symbolicSampleSize == Vst::kSample32)
+		return kResultTrue;
+	return kResultFalse;
+}
+
+void Processor::setDelay(unsigned nSamples)
+{
+	int temp = delayWrite - nSamples;
+
+	while (temp < 0)
+	{
+		temp += MAX_DELAY_BUFFER_SIZE;
+	}
+
+	delayRead = temp;
+}
+
+bool Processor::updateTempo(Vst::ProcessData& data)
+{
+
 	if (data.processContext && data.processContext->state & Vst::ProcessContext::kTempoValid)
 	{
 		if (data.processContext->tempo != tempo)
 		{
 			tempo = data.processContext->tempo;
-			samplesDelay = (60. / tempo) * time * 4 * processSetup.sampleRate;
-			delayWrite = samplesDelay;
-			delayRead = 0;
+			return true;
 		}
 	}
+	return false;
+}
+
+unsigned Processor::calculateDelay()
+{
+	return (60. / tempo) * time * 4 * processSetup.sampleRate;
+}
+
+void Processor::handleParameterChange(Vst::ProcessData& data)
+{
 	// Read inputs parameter changes
 	if (data.inputParameterChanges)
 	{
@@ -97,13 +156,7 @@ tresult PLUGIN_API Processor::process (Vst::ProcessData& data)
 						kResultTrue)
 					{
 						time = value;
-						unsigned t = (60. / tempo) * time * 4 * processSetup.sampleRate;
-						if (t > 0)
-						{
-							samplesDelay = t;
-							delayRead = 0;
-							delayWrite = samplesDelay;
-						}
+						calcSetDelay();
 					}
 					break;
 				case Params::dry:
@@ -132,57 +185,45 @@ tresult PLUGIN_API Processor::process (Vst::ProcessData& data)
 			}
 		}
 	}
+}
 
-	if (data.numInputs == 0 || data.numOutputs == 0)
-	{
-		return kResultOk;
-	}
-
-	void** in = getChannelBuffersPointer(processSetup, *data.inputs);
-	void** out = getChannelBuffersPointer(processSetup, *data.outputs);
-
-	int32 numChannels = data.inputs[0].numChannels;
-	uint32 sampleFramesSize = getSampleFramesSizeInBytes(processSetup, data.numSamples);
-
-	if (data.inputs[0].silenceFlags != 0)
-	{
-		data.outputs[0].silenceFlags = data.inputs[0].silenceFlags;
-
-		for (int32 i = 0; i < numChannels; i++)
-		{
-			if (in[i] != out[i])
-			{
-				memset(out[i], 0, sampleFramesSize);
-			}
-		}
-		return kResultOk;
-	}
-
-	if (data.numSamples > 0)
-	{
-		float* inRight = (float*)in[0];
-		float *outRight = (float*)out[0];
-		float* inLeft = (float*)in[1];
-		float* outLeft = (float*)out[1];
-		float mixed = 0;
-		for (int i = 0; i < data.numSamples; i++)
-		{
-			outLeft[i] = dry * inLeft[i] + wet * delayBufferLeft[delayRead];
-			outRight[i] = dry * inRight[i] + wet * delayBufferRight[delayRead];
-			mixed = (inLeft[i] + inRight[i]) / 2;
-			delayBufferLeft[delayWrite] = (1 - width) * mixed + delayBufferRight[delayRead] * feedback;
-			delayBufferRight[delayWrite] = width * mixed + delayBufferLeft[delayRead] * feedback;
-			delayWrite = (delayWrite + 1) % (samplesDelay * 2);
-			delayRead = (delayRead + 1) % (samplesDelay * 2);
-		}
-	}
-	return kResultOk;
+void Processor::calcSetDelay()
+{
+	setDelay(calculateDelay());
 }
 
 
-tresult PLUGIN_API Processor::canProcessSampleSize (int32 symbolicSampleSize)
+void Processor::handleSilenceFlags(void** in, void** out, Vst::ProcessData& data)
 {
-	if (symbolicSampleSize == Vst::kSample32)
-		return kResultTrue;
-	return kResultFalse;
+	uint32 sampleFramesSize = getSampleFramesSizeInBytes(processSetup, data.numSamples);
+	data.outputs[0].silenceFlags = data.inputs[0].silenceFlags;
+
+	for (int32 i = 0; i < data.inputs[0].numChannels; i++)
+	{
+		if (in[i] != out[i])
+		{
+			memset(out[i], 0, sampleFramesSize);
+		}
+	}
+}
+
+void Processor::processBuffer(void** in, void** out, int32 numSamples)
+{
+
+	float* inRight = (float*)in[0];
+	float* outRight = (float*)out[0];
+	float* inLeft = (float*)in[1];
+	float* outLeft = (float*)out[1];
+	float mixed = 0;
+
+	for (int i = 0; i < numSamples; i++)
+	{
+		outLeft[i] = dry * inLeft[i] + wet * delayBufferLeft[delayRead];
+		outRight[i] = dry * inRight[i] + wet * delayBufferRight[delayRead];
+		mixed = (inLeft[i] + inRight[i]) / 2;
+		delayBufferLeft[delayWrite] = (1 - width) * mixed + delayBufferRight[delayRead] * feedback;
+		delayBufferRight[delayWrite] = width * mixed + delayBufferLeft[delayRead] * feedback;
+		delayWrite = (delayWrite + 1) % MAX_DELAY_BUFFER_SIZE;
+		delayRead = (delayRead + 1) % MAX_DELAY_BUFFER_SIZE;
+	}
 }
--- src/processor.h
+++ src/processor.h
@@ -14,6 +14,8 @@ public:
 		return (Steinberg::Vst::IAudioProcessor*)new Processor; 
 	}
 
+	// OVERRIDE DEFAULT VST PROCESSOR METHODSS
+
 	Steinberg::tresult PLUGIN_API initialize (Steinberg::FUnknown* context) SMTG_OVERRIDE;
 	
 	Steinberg::tresult PLUGIN_API setActive (Steinberg::TBool state) SMTG_OVERRIDE;
@@ -21,6 +23,45 @@ public:
 	Steinberg::tresult PLUGIN_API canProcessSampleSize (Steinberg::int32 symbolicSampleSize) SMTG_OVERRIDE;
 
 	Steinberg::tresult PLUGIN_API process (Steinberg::Vst::ProcessData& data) SMTG_OVERRIDE;
+
+	/**
+	 * Sets the samplesDelay variable and creates an offset between "write" and "read" positions in the delay buffer.
+	 */
+	void setDelay(unsigned nSamples);
+
+	/**
+	 * Updates local tempo variable if it has changed in the host.
+	 * 
+	 * \returns True if the tempo has changed.
+	 */
+	bool updateTempo(Steinberg::Vst::ProcessData& data);
+
+	/**
+	 * Calculates current delay in samples based on "time" parameter and host's tempo.
+	 * 
+	 * \returns The calculated delay.
+	 */
+	unsigned calculateDelay();
+
+	/**
+	 * Handles any changes of plugin parameters.
+	 */
+	void handleParameterChange(Steinberg::Vst::ProcessData& data);
+
+	/**
+	 * Calculates and sets delay based on internal parameters.
+	 */
+	void calcSetDelay();
+
+	/**
+	 * Processes the sound signal buffer.
+	 */
+	void processBuffer(void** in, void** out, Steinberg::int32 numSamples);
+
+	/**
+	 * Handles the silence flags.
+	 */
+	void handleSilenceFlags(void** in, void** out, Steinberg::Vst::ProcessData& data);
 		
 protected:
 
